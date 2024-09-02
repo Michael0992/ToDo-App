34959e164eab4d9a4bd07895f8ee333ac8629eca
Michael0992 <32776851+Michael0992@users.noreply.github.com>
2024-09-02 07:51:52 +0200

    Update AlarmToggle.jsx

--- src/AlarmToggle.jsx
+++ src/AlarmToggle.jsx
@@ -11,7 +11,7 @@ function AlarmToggle({ notes }) {
 
   // useRef wird verwendet, um ein Audio-Objekt zu erstellen und zu speichern.
   // Das Audio-Objekt bleibt während der gesamten Lebensdauer der Komponente gleich, sodass es nicht bei jedem Rendern neu erstellt wird.
-  const audioRef = useRef(new Audio('https://github.com/Michael0992/ToDo-App/blob/main/src/assets/alarm-sound.mp3'));
+  const audioRef = useRef(new Audio('https://drive.google.com/uc?export=download&id=1Ci3ht5OQv481ueYfv8uKayt_wQ-e3Ake'));
 
   // useEffect-Hook, der einmal beim Initialisieren der Komponente ausgeführt wird.
   // Setzt die `loop`-Eigenschaft des Audio-Objekts auf `true`, sodass die Audiodatei in einer Schleife wiederholt abgespielt wird.
